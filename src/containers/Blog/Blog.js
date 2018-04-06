af7172938b9aadc050ed1c91aaaa219b826bc99e
vvd <vvd@norse.digital>
2018-04-06 18:06:41 +0300

    First commit.

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import Posts from './Posts/Posts';
+
+import './Blog.css';
+
+class Blog extends Component {
+    render() {
+       
+
+        return (
+            <div>
+                <div className="Blog">
+                    <header>
+                        <nav>
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/new-post">New Post</a></li>
+                            </ul>
+                        </nav>
+                    </header>
+                </div>
+
+                <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} />
+
+            </div>
+        );
+    }
+}
+
+export default Blog;
