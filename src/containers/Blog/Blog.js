a100665541d14314a954026f723e31b6c9596d52
vvd <vvd@norse.digital>
2018-04-10 14:53:42 +0300

    Adds switch to router for load one per time.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
-import './Blog.css'; 
+import './Blog.css';
 
 class Blog extends Component {
     render() {
-       
- 
+
+
         return (
             <div>
                 <div className="Blog">
@@ -31,13 +31,15 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch> 
 
             </div>
         );
     }
-} 
+}
 
 export default Blog;
