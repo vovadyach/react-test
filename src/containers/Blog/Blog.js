38b8677b59849c988e4417c206ee1fd1155b896f
vvd <vvd@norse.digital>
2018-04-10 14:43:10 +0300

    Adds router params to render id.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 import './Blog.css'; 
 
@@ -32,6 +33,7 @@ class Blog extends Component {
 
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
 
             </div>
         );
