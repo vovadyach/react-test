a844b9e2fac4a709aa995743445cc57f8382ea0a
vvd <vvd@norse.digital>
2018-04-10 12:16:44 +0300

    Implemetns switching between pages and adds params.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
-import './Blog.css';
+import './Blog.css'; 
 
 class Blog extends Component {
     render() {
        
-
+ 
         return (
             <div>
                 <div className="Blog">
                     <header>
                         <nav>
                             <ul>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/new-post">New Post</a></li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</Link></li>
                             </ul>
                         </nav>
                     </header>
@@ -26,10 +31,11 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
 
             </div>
         );
     }
-}
+} 
 
 export default Blog;
