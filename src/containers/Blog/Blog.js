f5a2c83a2b63e156225cfbb23d5fc7119512a3dd
vvd <vvd@norse.digital>
2018-04-11 11:48:09 +0300

    Adds hoc async component into chunk.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncConmponent';
+// import NewPost from './NewPost/NewPost';
 
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
+
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render() {
@@ -33,7 +38,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
 
                 <Switch>
-                    { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    { this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not found</h1>} />
                     { /* <Redirect from="/" to="/posts" /> */}
